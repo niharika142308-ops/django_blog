947b733b590059cb2c3d4bbad37cbfce199e82c8
Niharika p <niharika.p@soroco.com>
2021-06-03 18:12:51 +0530

    email reset implemented

---
+++ django_project/config.py
@@ -0,0 +1,2 @@
+user_name = "niharika.142308@gmail.com"
+user_password = "igujsnorbitywbqy"
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os 
+import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,8 +133,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
+EMAIL_HOST_USER = config.user_name
+EMAIL_HOST_PASSWORD = config.user_password 
 
